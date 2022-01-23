930a349c1e5cfbebd4efa41df1f08f902662b24d
Nguyen Trong Phuc <grayfptcs3@gmail.com>
2022-01-23 22:10:46 +0700

    redirect to Todo when user logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Todo from './pages/Todo';
 import 'material-icons/iconfont/material-icons.css';
 import PrivateRoute from './routes/PrivateRoute';
+import { useEffect } from 'react';
 
 const App = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const isSignedIn = localStorage.getItem('token');
+
+        if (isSignedIn) navigate('/');
+    }, []);
+
     return (
         <Routes>
             <Route
